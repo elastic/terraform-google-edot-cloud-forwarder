ef1b3410ce45306b119621fa0e20b0b0e1f6a6d8
Joe Caprino <joe.caprino@elastic.co>
2025-11-04 20:18:43 -0500

    Move secret, docker image, and cloud run service acct

--- modules/elastic-cloud-forwarder/main.tf
+++ modules/elastic-cloud-forwarder/main.tf
@@ -12,3 +12,34 @@
 # law.  Dissemination of this information or reproduction of
 # this material is strictly forbidden unless prior written
 # permission is obtained from Elasticsearch B.V.
+
+# We need a secret in which to save the API key from the serverless project.
+# Note that the "version" in google_secret_manager_secret_version actually
+# receives the secret_data
+resource "google_secret_manager_secret" "elastic_api_key" {
+  secret_id = "${var.ecf_asset_prefix}-elastic-api-key"
+  replication {
+    auto {}
+  }
+}
+
+# Resource to save the API key in the secret.
+resource "google_secret_manager_secret_version" "elastic_api_key" {
+  # Note that this is not the secret_id, but the secret's resource idenitifier,
+  # e.g. projects/{{project}}/secrets/{{secret_id}}
+  secret      = google_secret_manager_secret.elastic_api_key.id
+  secret_data = var.elastic_api_key
+}
+
+data "google_artifact_registry_docker_image" "ecf" {
+  image_name    = "${var.image_name}:latest"
+  location      = var.region
+  repository_id = var.registry_name
+}
+
+# Create a service account to use in the google cloud run to grant the least
+# amount of privileges.
+resource "google_service_account" "cloud_run" {
+  # limit to 30 characters long
+  account_id = substr("${var.ecf_asset_prefix}-cloud-run", 0, 30)
+}
--- modules/elastic-cloud-forwarder/outputs.tf
+++ modules/elastic-cloud-forwarder/outputs.tf
@@ -12,3 +12,24 @@
 # law.  Dissemination of this information or reproduction of
 # this material is strictly forbidden unless prior written
 # permission is obtained from Elasticsearch B.V.
+
+
+output "elastic_api_key_secret_id" {
+  description = "Elastic API key secret ID for exporting logs via the Elastic Cloud Forwarder."
+  value       = google_secret_manager_secret.elastic_api_key.secret_id
+}
+
+output "elastic_api_key_version" {
+  description = "Elastic API key version for exporting logs via the Elastic Cloud Forwarder."
+  value       = google_secret_manager_secret_version.elastic_api_key.version
+}
+
+output "ecf_google_artifact_registry_docker_image_self_link_uri" {
+  description = "URI of the ECF Docker image."
+  value       = data.google_artifact_registry_docker_image.ecf.self_link
+}
+
+output "ecf_cloud_run_service_account_email" {
+  description = "Email of the Cloud Run service account."
+  value       = google_service_account.cloud_run.email
+}
