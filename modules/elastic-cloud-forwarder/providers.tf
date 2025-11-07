2e60ed279b9e4a6b07c8ac046b57b731a5258db2
Joe Caprino <joe.caprino@elastic.co>
2025-11-07 10:33:54 -0500

    Add google to required providers

--- modules/elastic-cloud-forwarder/providers.tf
+++ modules/elastic-cloud-forwarder/providers.tf
@@ -16,6 +16,10 @@
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 7.0.0"
+    }
     docker = {
       source  = "kreuzwerker/docker"
       version = ">= 3.0"
